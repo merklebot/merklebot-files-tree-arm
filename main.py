ad281c5c65c1674b82ef9c7927af6d8643792740
Alex Smekhnov <alexander.smehnov@gmail.com>
2024-02-26 20:02:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 import os
 
-print("Start recording audio")
-sample_name = "aaaa.wav"
-cmd = f'arecord -vv --format=cd --device={os.environ["AUDIO_INPUT_DEVICE"]} -r 48000 --duration=10 -c 1 {sample_name}'
-print(cmd)
-os.system(cmd)
-print("Playing sound")
-os.system(f"ffplay -nodisp -autoexit -loglevel quiet {sample_name}")
+DATA_PATH= "/merklebot/job_data"
 
-# Capture image
-import cv2
-camera_capture = cv2.VideoCapture(0)
-rv, image = camera_capture.read()
-print(f"Image Dimensions: {image.shape}")
-camera_capture.release()
+def create_text_file(path, content):
+  with open(path, 'w') as f:
+    f.write(content)
+
+
+"""
+Generate file tree
+-path
+  - f0.txt
+  - dir1
+    - f1-0.txt
+    - f1-1.txt
+    - dir2
+      - f2-0.txt
+      - dir3
+        - f3-0.txt
+  - dir4
+    - f4-0.txt
+"""
+def generate_tree(path):
+  create_text_file(f"{path}/f0.txt", "Content of f0.txt")
+  
+  os.makedirs(f"{path}/dir1/dir2/dir3", exist_ok=True)
+  create_text_file(f"{path}/ditr1/f1-0.txt", "Content of f1-0.txt")
+  create_text_file(f"{path}/ditr1/f1-1.txt", "Content of f1-1.txt")
+  create_text_file(f"{path}/ditr1/dir2/f2-0.txt", "Content of f2-0.txt")
+  create_text_file(f"{path}/ditr1/dir3/f3-0.txt", "Content of f3-0.txt")
+  
+  os.makedirs(f"{path}/dir4", exist_ok=True)
+  create_text_file(f"{path}/ditr4/f4-0.txt", "Content of f4-0.txt")
+
+if __name__=='__main__':
